e0b2fff50ec1790522a04c894e0eff9ed255a195
Hilmar <hilmar@hilmarp.com>
2014-12-30 16:19:13 +0000

    Getur nad i gogn og vistad i json skra a hdd

--- server.js
+++ server.js
@@ -5,9 +5,57 @@ var express = require('express'),
 	cheerio = require('cheerio'),
 	app     = express();
 
-app.get('/scrape', function(req, res) {
+app.get('/leiga', function(req, res) {
 
+	// Náum í fasteignir frá þessu URLi
+	url = 'http://www.mbl.is/leiga/';
 
+	// Keyrum request
+	request(url, function(error, response, html) {
+
+		if (!error) {
+
+			// Notum Cheerio á HTML sem kemur til baka
+			var $ = cheerio.load(html);
+
+			// Upplýsingar sem við erum að ná í
+			var address, price;
+			var json = { address : "", price : "" };
+
+			// Ná í address
+			$('.rental-itemlist-property .rental-itemlist-headline').filter(function() {
+
+				// Vista address
+				address = $(this).text();
+
+				// Bæta því í json object
+				json.address = address;
+
+			});
+
+			// Ná í price
+			$('.rental-itemlist-property .rental-itemlist-price').filter(function() {
+
+				// Vista price
+				price = $(this).text();
+
+				// Bæta því í json object
+				json.price = price;
+
+			});
+
+		}
+
+		// Búa til json skrá með upplýsingunum af síðunni
+		fs.writeFile('leiga.json', JSON.stringify(json, null, 4), function(err) {
+
+			console.log('Skrá hefur verið búin til!');
+
+		});
+
+		res.send('Gáðu í console');
+
+	});
 
 });
 
